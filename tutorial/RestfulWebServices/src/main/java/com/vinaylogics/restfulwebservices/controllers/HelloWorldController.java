16f0b04ade23a90d403071c3d010fee4f834dc30
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-10-14 01:39:00 +0530

    Path Variable

--- tutorial/RestfulWebServices/src/main/java/com/vinaylogics/restfulwebservices/controllers/HelloWorldController.java
+++ tutorial/RestfulWebServices/src/main/java/com/vinaylogics/restfulwebservices/controllers/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.vinaylogics.restfulwebservices.controllers;
 
 import com.vinaylogics.restfulwebservices.models.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorld helloWorldBean(){
         return new HelloWorld("Hello World");
+    }
+
+    ///hello-world/path-variable/vinay
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorld helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorld(String.format("Hello World, %s", name));
     }
 }

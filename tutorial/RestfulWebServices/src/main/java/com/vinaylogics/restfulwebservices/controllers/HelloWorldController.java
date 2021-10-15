b2603c2922975c8bb239ae8243827f39be844d03
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-10-15 19:56:12 +0530

    i18n support added to hello world api

--- tutorial/RestfulWebServices/src/main/java/com/vinaylogics/restfulwebservices/controllers/HelloWorldController.java
+++ tutorial/RestfulWebServices/src/main/java/com/vinaylogics/restfulwebservices/controllers/HelloWorldController.java
@@ -1,15 +1,20 @@
 package com.vinaylogics.restfulwebservices.controllers;
 
 import com.vinaylogics.restfulwebservices.models.HelloWorld;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 // Controller
 @RestController
 @RequestMapping()
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     // GET
     // URI - /hello-world
@@ -28,5 +33,19 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorld helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorld(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world-internalized")
+    public String helloWorldInternationalized(
+            //@RequestHeader(name = "Accept-Language",required = false) Locale locale
+    ){
+        // return "Hello World";
+        // en = Hello World
+        // nl = Hallo Wereld
+        // fr = Bonjour le monde
+        return messageSource.getMessage("good.morning.message",null, "Default Message" ,
+                //locale
+                LocaleContextHolder.getLocale()
+        );
     }
 }

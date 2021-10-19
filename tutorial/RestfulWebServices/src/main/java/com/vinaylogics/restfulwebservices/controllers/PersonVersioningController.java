b686c8bd262e7d46266768cbf212065877068a40
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-10-20 01:32:19 +0530

    Param And Content Negotiation Versioning

--- tutorial/RestfulWebServices/src/main/java/com/vinaylogics/restfulwebservices/controllers/PersonVersioningController.java
+++ tutorial/RestfulWebServices/src/main/java/com/vinaylogics/restfulwebservices/controllers/PersonVersioningController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
+//    URI VERSIONING
     @GetMapping("v1/person")
     public PersonV1 personV1(){
         return new PersonV1("Vinay Ganesh");
@@ -18,4 +19,38 @@ public class PersonVersioningController {
     public PersonV2 personV2(){
         return new PersonV2(new Name("Vinay", "Ganesh"));
     }
+
+//    REQUEST PARAM VERSIONING
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 paramV1(){
+        return new PersonV1("Vinay Ganesh");
+    }
+
+    @GetMapping(value = "person/param", params = "version=2")
+    public PersonV2 paramV2(){
+        return new PersonV2(new Name("Vinay", "Ganesh"));
+    }
+
+//    REQUEST HEADER VERSIONING
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 headerV1(){
+        return new PersonV1("Vinay Ganesh");
+    }
+
+    @GetMapping(value = "person/header", headers = "X-API-VERSION=2")
+    public PersonV2 headerV2(){
+        return new PersonV2(new Name("Vinay", "Ganesh"));
+    }
+
+//   CONTENT NEGOTIATION VERSIONING
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1(){
+        return new PersonV1("Vinay Ganesh");
+    }
+
+    @GetMapping(value = "person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2(){
+        return new PersonV2(new Name("Vinay", "Ganesh"));
+    }
+
 }
